fd8468ce1f44cd685051f6b8fad9837ba266874f
Slav Mosoyan <vladanik1999@gmail.com>
2026-03-23 18:32:25 +0400

    fx server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,26 @@
+// eslint-disable-next-line no-undef
 const express = require("express");
+// eslint-disable-next-line no-undef
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const tracks = [
+  { id: 1, title: "Track 1", artist: "Artist 1" },
+  { id: 2, title: "Track 2", artist: "Artist 2" },
+  { id: 2, title: "Track 2", artist: "Artist 2" },
+  { id: 2, title: "Track 2", artist: "Artisывывt 2" },
+  { id: 2, title: "Track 2", artist: "Artisывывt 2e" },
+];
+
 app.get("/", (req, res) => {
   res.send("Server is working 🚀");
+});
+
+app.get("/tracks", (req, res) => {
+  res.json(tracks);
 });
 
 const PORT = 5000;
