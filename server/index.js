3e675f0bccfd7ae5972d2891f369be79b5221600
Slav Mosoyan <vladanik1999@gmail.com>
2026-03-25 16:31:24 +0400

    add music

--- server/index.js
+++ server/index.js
@@ -6,11 +6,27 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 const tracks = [
-  { id: 1, title: "Track 1", artist: "G-Eazy & Halsey - Him & I" },
-  { id: 2, title: "Track 2", artist: "Juice WRLD - Lucid Dreams" },
-  { id: 2, title: "Track 2", artist: "Justin Timberlake - Mirrors" },
+  {
+    id: 1,
+    title: "Track 1",
+    artist: "G-Eazy & Halsey - Him & I",
+    src: "http://localhost:5000/songs/track1.mp3",
+  },
+  {
+    id: 2,
+    title: "Track 2",
+    artist: "Juice WRLD - Lucid Dreams",
+    src: "http://localhost:5000/songs/track2.mp3",
+  },
+  {
+    id: 3,
+    title: "Track 3",
+    artist: "Justin Timberlake - Mirrors",
+    src: "http://localhost:5000/songs/track3.mp3",
+  },
 ];
 
 app.get("/", (req, res) => {
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import { useEffect, useState } from "react";
-
 import "./App.css";
 import axios from "axios";
 
+const audioPlayer = new Audio();
+
 function App() {
   const [tracks, setTracks] = useState([]);
+  const [currentTrack, setCurrentTrack] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
 
+  const playTrack = (track) => {
+    if (currentTrack?.id === track.id) {
+      // Если нажали на тот же трек — ставим на паузу или играем снова
+      if (isPlaying) {
+        audioPlayer.pause();
+        setIsPlaying(false);
+      } else {
+        audioPlayer.play();
+        setIsPlaying(true);
+      }
+    } else {
+      // Если новый трек — меняем источник и запускаем
+      setCurrentTrack(track);
+      // eslint-disable-next-line react-hooks/immutability
+      audioPlayer.src = track.src;
+      audioPlayer.play();
+      setIsPlaying(true);
+    }
+  };
   useEffect(() => {
     const fetchTracks = async () => {
       try {
@@ -29,27 +51,35 @@ function App() {
 
         <div className="track-list">
           {tracks.map((track) => (
-            <div key={track.id} className="track-id">
+            <div
+              key={track.id}
+              onClick={() => playTrack(track)}
+              className="track-id"
+            >
               <div className="track-info">
                 <h3>{track.title}</h3>
                 <h3>{track.artist}</h3>
               </div>
-              <button
-                className="play-btn"
-                onClick={() => alert(`Играем ${track.title}`)}
-              >
-                ▶
+              <button className="play-btn">
+                {currentTrack?.id === track.id && isPlaying ? "⏸" : "▶"}
               </button>
             </div>
           ))}
         </div>
-        <div className="mini-player">
-          <div className="track-info">
-            <h3>Ничего не выбрано</h3>
-            <p>Нажми Play</p>
+        {currentTrack && (
+          <div className="mini-player">
+            <div className="track-info">
+              <h3>{currentTrack.title}</h3>
+              <p>{currentTrack.artist}</p>
+            </div>
+            <button
+              className="play-btn"
+              onClick={() => playTrack(currentTrack)}
+            >
+              {isPlaying ? "⏸" : "▶"}
+            </button>
           </div>
-          <button className="play-btn">Ⅱ</button>
-        </div>
+        )}
       </div>
     </>
   );
